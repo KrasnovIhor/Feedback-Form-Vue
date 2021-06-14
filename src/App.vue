13434e1971a7f7db9fb9d1a9c8ff1ff7e66b2b38
Ihor Krasnov <krasnov@bramblingtech.com>
2021-06-14 19:15:47 +0300

    removed console logs and comments

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,6 @@
   <div class="container h-100">
     <FeedbackForm />
   </div>
-  <!-- <form action="" class="mx-auto">
-    <div id="app" class="container col-sm-10 mx-auto">
-      <h1 class="pt-4 mb-5">Feedback Form</h1>
-      <div class="mb-4">
-        <input
-          type="text"
-          class="form-control"
-          id="exampleFormControlInput1"
-          placeholder="Name"
-          v-model.trim="name"
-        />
-      </div>
-      <div class="mb-4">
-        <input
-          type="email"
-          class="form-control"
-          id="exampleFormControlInput1"
-          placeholder="Email"
-          v-model.trim="email"
-        />
-      </div>
-      <div class="mb-5">
-        <textarea
-          class="form-control textarea"
-          id="exampleFormControlTextarea1"
-          rows="3"
-          placeholder="Comments"
-          v-model="comment"
-        ></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary mb-4">Send Feedback</button>
-    </div>
-  </form> -->
 </template>
 
 <script>
